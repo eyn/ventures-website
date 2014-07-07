ccf388c743565191a2d6acf2892aa86716f2ce20
David Blurton <david.blurton@gmail.com>
2014-07-07 21:15:25 +0100

    Add autoprefixer

--- Brocfile.js
+++ Brocfile.js
@@ -2,12 +2,15 @@ var jade = require('broccoli-jade');
 var mergeTrees = require('broccoli-merge-trees');
 var pickFiles = require('broccoli-static-compiler');
 var compileSass = require('broccoli-sass');
+var autoprefixer = require('broccoli-autoprefixer');
 
 var html = jade('templates', {
   basedir: 'templates'
 });
 
 var css = compileSass(['styles'], 'main.scss', 'style.css', {sourceMap: true, outputStyle: 'compressed'});
+
+css = autoprefixer(css);
 
 var assets = pickFiles('public', {
   srcDir: '/',
