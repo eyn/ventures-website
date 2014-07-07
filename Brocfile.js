d06ce3dcc1077939e5a9be635b9c753cd56f2292
David Blurton <david.blurton@gmail.com>
2014-07-07 20:19:42 +0100

    Build jade and sass using broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,52 +1,17 @@
-/* global require, module */
+var jade = require('broccoli-jade');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
+var compileSass = require('broccoli-sass');
 
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-
-var app = new EmberApp({
-  name: require('./package.json').name,
-
-  // for some large projects, you may want to uncomment this (for now)
-  es3Safe: true,
-
-  minifyCSS: {
-    enabled: true,
-    options: {}
-  },
-
-  getEnvJSON: require('./config/environment')
+var html = jade('templates', {
+  basedir: 'templates'
 });
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+var css = compileSass(['styles'], 'main.scss', 'style.css');
 
-app.import({
-  development: 'vendor/ember-data/ember-data.js',
-  production:  'vendor/ember-data/ember-data.prod.js'
-}, {
-  'ember-data': [
-    'default'
-  ]
-});
+var assets = pickFiles('public', {
+  srcDir: '/',
+  destDir: '/'
+})
 
-app.import('vendor/ic-ajax/dist/named-amd/main.js', {
-  'ic-ajax': [
-    'default',
-    'defineFixture',
-    'lookupFixture',
-    'raw',
-    'request',
-  ]
-});
-
-
-module.exports = app.toTree();
+module.exports = mergeTrees([html, css, assets]);
